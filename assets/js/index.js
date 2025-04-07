0aeab1575d8220ac562fcaf06c6922de4c002d9e
jony0407 <jonyestomeloca@gmail.com>
2025-04-07 23:14:46 +0500

    Main

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".gmx-swiper", {
+    direction: "horizontal",
+    spaceBetween: 52,
+    slidesPerView: 6.5,
+    speed: 3000,
+    loop: true,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      576: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+      1400: {
+        slidesPerView: 6,
+        spaceBetween: 52,
+      },
+    },
+  });
+});
+
 let navOverlay = document.querySelector(".nav__overlay");
 let burgerBtn = document.querySelector(".header__burger-btn");
-
+const isNavActive = () => navOverlay.classList.contains("active");
 burgerBtn.addEventListener("click", () => {
   navOverlay.classList.toggle("active");
   burgerBtn.classList.toggle("active");
-  document.body.style.overflow = "hidden";
+
+  document.body.style.overflow = isNavActive() ? "hidden" : "unset";
 });
 
 navOverlay.addEventListener("click", (evt) => {

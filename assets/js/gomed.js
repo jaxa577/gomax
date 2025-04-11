ca42d7c8d83b6ed83293318f0d05bf91cae41f73
jony0407 <jonyestomeloca@gmail.com>
2025-04-11 22:42:14 +0500

    ыфвфв

--- assets/js/gomed.js
+++ assets/js/gomed.js
@@ -107,3 +107,39 @@ function renderContent(tabId) {
 
 renderButtons();
 renderContent("tab1");
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".gmx-swiper", {
+    direction: "horizontal",
+    spaceBetween: 52,
+    slidesPerView: 6.5,
+    speed: 3000,
+    loop: true,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      576: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+      1400: {
+        slidesPerView: 6,
+        spaceBetween: 52,
+      },
+    },
+  });
+});
